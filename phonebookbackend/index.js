5612798f7166fbd604b13e862f83eb96ba36e1e2
markrenssi <mark.renssi@aalto.fi>
2023-06-28 20:14:59 +0300

    Connected the backend to a database

--- phonebookbackend/index.js
+++ phonebookbackend/index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
 
 const app = express()
 
@@ -43,10 +45,16 @@ let persons = [
 ]
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then(persons => {
+    response.json(persons)
+  }) 
 })
 
 app.get('/api/persons/:id', (request, response) => {
+  Person.findById(request.params.id).then(person => {
+    response.json(person)
+  })
+  /*
   const id = Number(request.params.id)
   const person = persons.find(person => person.id === id)
   if(person) {
@@ -54,6 +62,7 @@ app.get('/api/persons/:id', (request, response) => {
   } else {
     response.status(404).end()
   }
+  */
 })
 
 app.get('/api/info', (request, response) => {
@@ -89,25 +98,23 @@ app.post('/api/persons', (request, response) => {
     })
   }
 
-  const isUnique = persons.map(person => person.name).includes(body.name)
-  
-  if(isUnique) {
-    return response.status(400).json({ 
-      error: 'name must be unique'
-    })
-  }
-
-  const person = {
-    id: Math.floor(Math.random() * 1000),
-    name: body.name,
-    number: body.number
-  }
-  
-  persons = persons.concat(person)
-
-  response.json(person)
+  Person.find({name: body.name}).then(result => {
+    if(result.length === 0) {  // name is unique
+      const person = new Person({
+        name: body.name,
+        number: body.number
+      })
+      person.save().then(savedPerson => {
+        response.json(savedPerson)
+      })
+    } else {  // name is not unique
+      return response.status(400).json({ 
+        error: 'name must be unique'
+      })
+    }
+  })
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
